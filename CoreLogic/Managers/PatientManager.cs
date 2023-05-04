2ea1a698483e257484e1a54947427d9f07b2d602
selwavalencia@gmail.com <selwavalencia@gmail.com>
2023-05-04 02:25:38 -0400

    P3-001 Implemented GetById method to PatientManager.cs file

--- CoreLogic/Managers/PatientManager.cs
+++ CoreLogic/Managers/PatientManager.cs
@@ -38,4 +38,21 @@ public class PatientManager
         int index = random.Next(0,bloodGroups.Length);
         return bloodGroups[index];
     }
+
+     public Patient GetById(int ci)
+    {
+        if(ci < 0)
+        {
+            throw new Exception("CI invalido");
+        }
+
+        Patient patientFound;
+        patientFound = _patients.Find(patient => patient.CI == ci);
+
+        if(patientFound == null)
+        {
+            throw new Exception("No se encontró ningún paciente con el CI: " + ci);
+        }
+        return patientFound;
+    }
 }
