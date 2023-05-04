696df9383f7fe55b0f6c5f4c0819251a5b8d5975
selwavalencia@gmail.com <selwavalencia@gmail.com>
2023-05-04 01:44:17 -0400

    P3-001 Implemented Create and GetBloodType methods in PatientManager.cs file.

--- CoreLogic/Managers/PatientManager.cs
+++ CoreLogic/Managers/PatientManager.cs
@@ -15,4 +15,27 @@ public class PatientManager
     {
         return _patients;
     }
+
+    public Patient Create(string name, string lastName, int ci)
+    {
+        string bloodType = GetBloodType();
+
+        Patient createdPatient = new Patient()
+        {
+            Name = name,
+            LastName = lastName,
+            CI = ci,
+            BloodType = bloodType
+        };
+        _patients.Add(createdPatient);
+        return createdPatient;
+    }
+
+    public string GetBloodType()
+    {
+        string[] bloodGroups = new string[] {"A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"};
+        Random random = new Random();
+        int index = random.Next(0,bloodGroups.Length);
+        return bloodGroups[index];
+    }
 }
