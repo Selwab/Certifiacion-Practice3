886198f8709e0a0059a61d9a5115c62e156826da
selwavalencia@gmail.com <selwavalencia@gmail.com>
2023-05-04 00:06:49 -0400

    P3-001 Added constructor and setted list of patients as attribute to PatientManager.cs.

--- CoreLogic/Managers/PatientManager.cs
+++ CoreLogic/Managers/PatientManager.cs
@@ -1,6 +1,13 @@
+using UPB.CoreLogic.Models;
+
 namespace UPB.CoreLogic.Managers;
 
 public class PatientManager
 {
-    
+    private List<Patient> _patients;
+
+    public PatientManager()
+    {
+        _patients = new List<Patient>();
+    }
 }
